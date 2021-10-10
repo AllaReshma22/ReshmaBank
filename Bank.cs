6b56b9dad9e0aa798775aadac13295612e0ec3a6
AllaReshma22 <91258492+AllaReshma22@users.noreply.github.com>
2021-10-10 18:35:29 +0530

    Add files via upload

---
+++ Bank.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BankApp.Models
+{
+    
+    public class Account
+    {
+        public int AccountNumber { get; set; }
+        public string AccountHolderName { get; set; }
+       
+        public int pin { get; set; }
+        public int Balance { get; set; }
+
+        }
+    public class Transaction
+    {
+        public Transaction(int toid, string type, int amount, string datetime)
+        {
+            this.toid = toid;
+            Type = type;
+            Amount = amount;
+            Datetime = datetime;
+        }
+        public int toid { get; set; }
+        public int fromid { get; set; }
+        public string Type { get; set; }   
+        public int Amount { get; set; }
+        public string Datetime { get; set; }
+       
+    }
+    }
+
